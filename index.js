86d8654c58fc15b754c6f43be6977b0ec5515140
Tarun Singh <newtarunsh@gmail.com>
2025-01-08 23:39:56 +0000

    adding node and cache files in gitignore

--- index.js
+++ index.js
@@ -2,23 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // Write your React code here
-const element = React.createElement("h1", null, "Hello, React Using CDN!");
 
 // Render the element into the root div
-const interEle = ()=>{
-    return(
-        <h3>This is inner element</h3>
-    )
-}
 
-
-const renderEle = ()=>{
-    return(
-        <h1>This is the component trial</h1>
-        <h3>We can add many sub-headings as well</h3>
-        <interEle/>
-    )
-}
+const RenderEle = () => {
+  return (
+    <div>
+      <h1>This is the component trial</h1>
+      <h3>We can add many sub-headings as well</h3>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<RenderEle />);
