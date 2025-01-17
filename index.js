1999a743f0976062e121ea3d699c8b62a533c70e
Tarun Singh <newtarunsh@gmail.com>
2025-01-17 09:27:59 +0000

    added restaurant file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Restaurants from "./Constants/Restaurants";
 
 // Write your React code here
 
@@ -10,6 +11,7 @@ const RenderEle = () => {
     <div>
       <h1>This is the component trial</h1>
       <h3>We can add many sub-headings as well</h3>
+      <h4>{Restaurants[0].price_range}</h4>
     </div>
   );
 };
